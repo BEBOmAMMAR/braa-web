d0f161028fd2d7b7f5211fe7a0d9680efc760fd9
BEBOmAMMAR <44676135+BEBOmAMMAR@users.noreply.github.com>
2025-04-21 11:07:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => console.log("Service Worker مسجل", reg))
+      .catch(err => console.error("فشل التسجيل", err));
+  });
+}
